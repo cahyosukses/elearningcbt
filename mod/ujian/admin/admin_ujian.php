d8f358a0835cb757eddb4f4a1e8d412aae47e329
rekysyarifudin <reky@teamworks.co.id>
2015-11-04 14:21:58 +0700

    17/10/2015

--- mod/ujian/admin/admin_ujian.php
+++ mod/ujian/admin/admin_ujian.php
@@ -87,7 +87,7 @@ $waktuakhir = $_SESSION['waktuakhir'];
 }else{
 $_SESSION['waktuakhir']= $waktuakhir;	
 }
-/*
+
 $JS_SCRIPT.= <<<js
 <script type="text/javascript" src="includes/countdown2/jquery.countdownTimer.js"></script>
 <script>
@@ -101,48 +101,7 @@ $JS_SCRIPT.= <<<js
   });
 </script>
 js;
-*/
-?>
-<script>
-  settab();
-
-  var t=<?php
-  /* 
-  kalo tanda // di bawah dihapus, 
-  biarpun di refresh, sisa waktu kagak bakal balik ke asal 20 detik
-  */
-  //if(!isset($_SESSION['timer']))
-$_SESSION['timer']=date('U');
-  /* waktu cuma di set 20 detik */
-  echo 20-(date('U')-$_SESSION['timer']); ?>;
-  var t1=0;
-  var t2=0;
-  var d=document.getElementById("timer");
-
-  function timer() {
-    if(t<=0) {
-      clearInterval(vtimer);
-      document.getElementById("timeout").style.display="block";
-      document.getElementById("submit").click();
-    }
-    t1=t;
-    t2=t1%60;
-    t1=(t1-t2)/60;
-    t1=(t1<10?"0":"")+t1;
-    t2=(t2<10?"0":"")+t2;
-    d.innerHTML=t1+":"+t2;
-    t-=1;
-  }
-  timer();
-
-  var vtimer=setInterval(function(){timer()},1000);
-</script>
-<?php
-$time6 = time();
-$dateu = date('U');
-echo '$time6 : '.$time6.'dateu : '.$dateu.', session :'.$_SESSION['timer'];
 }
-
 $script_include[] = $JS_SCRIPT;
 $user =  $_SESSION['UserName'];
 $levelakses=$_SESSION['LevelAkses'];
@@ -1028,8 +987,7 @@ $petunjukumum = "
 ";
 }
 
-//$timercountdown = '<tr><td colspan="6"><div id="future_date"></div></td></tr>';
-$timercountdown = '<tr><td colspan="6"><div id="timeout">Time Out !!!</div>';
+$timercountdown = '<tr><td colspan="6"><div id="future_date"></div></td></tr>';
 $admin .= '
 <table cellspacing="0" cellpadding="0"class="table table-striped table-hover">
 	<tr>
